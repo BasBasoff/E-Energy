31f078808b549df837fe9871104863861d7e8595
Daniil Oshchepkow <oshepkow.daniil@yandex.ru>
2021-02-26 15:56:33 +0700

    Правка

--- E-Energy/app/static/app/scripts/script.js
+++ E-Energy/app/static/app/scripts/script.js
@@ -64,6 +64,6 @@ $('.chart').each(function () {
 tarif.onchange = function () {
 	for (str of tbody.children) {
 		str.children['XP_cost'].innerHTML = tarif.value * str.children['XP'].innerHTML
-		str.children['XP_percent'].innerHTML = (str.children['XP_cost'].innerHTML / (str.children['XP']*tarif.value))*100
+		str.children['XP_percent'].innerHTML = (str.children['XP_cost'].innerHTML / (str.children['XP'].innerHTML*tarif.value))*100
     }
 }
