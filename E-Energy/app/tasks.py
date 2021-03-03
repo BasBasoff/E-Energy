0b99cba21983d9713d30ddd48666cef21eaa9fb4
RXcor <platlonist@mail.ru>
2021-03-03 15:21:18 +0700

    РАбочий вариант

--- E-Energy/app/tasks.py
+++ E-Energy/app/tasks.py
@@ -1,7 +1,16 @@
 from E_Energy.celeryapp import app
 from .models import CachingData, Data, AdapterParameters, Records
 from datetime import datetime
+from django.forms.models import model_to_dict
 
+
+def preparation_dict(instance):
+    dict = model_to_dict(instance)
+    dict['record'] = Records.objects.get(id_record=dict.pop('id_record'))
+    dict['parameter'] = AdapterParameters.objects.get(
+        id_parameter=dict.pop('id_parameter')
+    )
+    return dict
 
 @app.task
 def data_caching():
@@ -18,13 +27,21 @@ def data_caching():
         new_data_instances = list(
             Data.objects.filter(id_record=record_instance.id_record)
         )
+        print(new_data_instances)
 
-        CachingData.objects.bulk_create(
-            [
-                CachingData(
-                    record_time=record_instance.record_time,
-                    adapter_id=record_instance.id_adapter,
-                    **data_instance
-                ) for data_instance in new_data_instances
-            ]
-        )
+        for data_instance in new_data_instances:
+            CachingData.objects.create(
+                record_time=record_instance.record_time,
+                adapter_id=record_instance.id_adapter.id_adapter,
+                **preparation_dict(data_instance)
+            )
+
+        # CachingData.objects.bulk_create(
+        #     [
+        #         CachingData(
+        #             record_time=record_instance.record_time,
+        #             adapter_id=record_instance.id_adapter,
+        #             **data_instance
+        #         ) for data_instance in new_data_instances
+        #     ]
+        # )
