f86111005338ce57fdada424d3c6c48e93d50469
RXcor <platlonist@mail.ru>
2021-03-03 13:07:45 +0700

    Отладка

--- E-Energy/app/tasks.py
+++ E-Energy/app/tasks.py
@@ -1,10 +1,15 @@
 from E_Energy.celeryapp import app
 from .models import CachingData, Data, AdapterParameters, Records
+from datetime import datetime
 
 
 @app.task
 def data_caching():
-    last_caching_record = CachingData.objects.last().record
+    try:
+        last_caching_record = CachingData.objects.last().record
+    except AttributeError:
+        last_caching_record = Records(record_tme=datetime(2000, 1,1,0,1))
+
     new_record_instances = Records.filter(
         record_time__gt=last_caching_record.record_time
     )
