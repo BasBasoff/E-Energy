f09eec6f85089deb553103d911cf993115ad54d8
RXcor <platlonist@mail.ru>
2021-03-05 14:04:10 +0700

    'Update'

--- E-Energy/app/tasks.py
+++ E-Energy/app/tasks.py
@@ -6,28 +6,32 @@ from django.forms.models import model_to_dict
 
 def preparation_dict(instance):
     dict = model_to_dict(instance)
-    dict['record'] = Records.objects.get(id_record=dict.pop('id_record'))
-    dict['parameter'] = AdapterParameters.objects.get(
-        id_parameter=dict.pop('id_parameter')
-    )
+    dict['record_id'] = dict.pop('id_record')
+    dict['parameter_id'] = dict.pop('id_parameter')
     return dict
 
-@app.task
+@app.task(time_limit=115)
 def data_caching():
-    try:
-        last_caching_record = CachingData.objects.last().record
-    except AttributeError:
-        last_caching_record = Records(record_time=datetime(2000, 1,1,0,1))
+    #try:
+    last_record_id = CachingData.objects.latest('record_time').record_id
+    print(last_record_id)
+    print(CachingData.objects.latest('record_time').record_time)
+    last_caching_record = Records.objects.get(id_record = last_record_id )
+    print(last_caching_record.record_time)
+    #except AttributeError:
+    #    last_caching_record = Records(record_time=datetime(2020, 1,26,0,1))
 
     new_record_instances = Records.objects.filter(
         record_time__gt=last_caching_record.record_time
-    )
+    ).order_by('record_time')
 
-    for record_instance in new_record_instances.iterator():
+    for record_instance in new_record_instances.iterator(chunk_size=50):
         new_data_instances = list(
             Data.objects.filter(id_record=record_instance.id_record)
         )
-        print(new_data_instances)
+        print(record_instance.id_record)
+        print(record_instance.record_time)
+		
 
         for data_instance in new_data_instances:
             CachingData.objects.create(
@@ -41,7 +45,7 @@ def data_caching():
         #         CachingData(
         #             record_time=record_instance.record_time,
         #             adapter_id=record_instance.id_adapter,
-        #             **data_instance
+        #             **preparation_dict(data_instance)
         #         ) for data_instance in new_data_instances
         #     ]
         # )
